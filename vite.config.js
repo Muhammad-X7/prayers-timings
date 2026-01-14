b0dd38c975484ecdec33a5b6c194eb9aaed4dbfe
Muhammad-X7 <muhamadhareth@gmail.com>
2026-01-14 21:35:20 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer({ open: true, gzipSize: true })],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("@mui")) return "mui";
-            if (id.includes("react") || id.includes("react-dom"))
-              return "react-vendor";
-            if (id.includes("axios")) return "axios";
-            if (id.includes("moment")) return "moment"; // عزل moment في chunk منفصل
-            return "vendor";
-          }
-        },
-      },
-    },
-    // إضافة هذه الخيارات لتحسين التوافق
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true,
-    },
-  },
+  plugins: [react()],
   optimizeDeps: {
-    include: ["react", "react-dom", "@mui/material", "axios", "moment"],
-    esbuildOptions: {
-      target: "esnext",
-    },
+    exclude: ["@popperjs/core"],
   },
 });
